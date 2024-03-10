feb0aecc7c37bc0c954ebf31918c39d87cd8057c
Alim1899 <alimakhchanogli@gmail.com>
2024-03-10 15:54:47 +0400

    Updated styling 'Projects' and 'About' page

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -4,27 +4,36 @@ import Navbar from "../../Navbar/Navbar";
 import imageOne from "../../../assets/photos/lazy.jpg";
 const About = () => {
   const lorem =
-    "Consectetur quis eiusmod ex laborum. Ullamco sit consequat pariatur laboris exercitation id labore id culpa exercitation adipisicing laborum excepteur ex. Minim fugiat laboris cillum veniam sit enim ad veniam fugiat. Cupidatat pariatur officia proident culpa laborum sit enim amet cupidatat. Consequat cillum aliqua est incididunt ut sint laborum consequat reprehenderit laborum consequat et duis. Id reprehenderit dolore et ullamco consequat aliquip. Adipisicing ex laboris aliquip qui amet anim nisi. Lorem laborum labore deserunt eu magna reprehenderit cupidatat culpa veniam magna et amet. Magna est eiusmod excepteur anim elit eu tempor velit sunt excepteur sit cillum anim. Dolore laborum laboris dolor aliquip. Minim magna sint quis irure reprehenderit fugiat fugiat dolore id deserunt est pariatur. Magna culpa dolore laborum Lorem aliqua tempor cillum officia tempor. Mollit ex consequat proident nisi ipsum quis sit reprehenderit ea deserunt deserunt tempor enim enim.";
+    "Officia proident culpa laborum sit enim amet cupidatat. Consequat cillum  consequat reprehenderit laborum consequat et duis. Id reprehenderit dolore et ullamco consequat aliquip. Adipisicing ex laboris aliquip qui amet anim nisi. Lorem laborum labore deserunt eu magna reprehenderit cupidatat culpa veniam magna et amet. Magna est eiusmod excepteur anim elit eu tempor velit sunt excepteur sit cillum anim. Dolore laborum laboris dolor aliquip. Minim magna sint quis irure reprehenderit fugiat fugiat dolore id deserunt est pariatur. Magna culpa dolore laborum Lorem aliqua tempor cillum officia tempor. Mollit ex consequat proident nisi ipsum quis sit reprehenderit ea deserunt deserunt tempor enim enim.";
   return (
     <div className={classes.main}>
       <Navbar />
-      <div className={classes.section}>
+      <div className={classes.content}>
+         <div className={classes.section}>
         <img src={imageOne} className={classes.img} alt="img"></img>
-        <p className={classes.description}>
-         {lorem}
-        </p>
+        <div className={classes.description}>
+        <p className={classes.text}>{lorem}</p>
+         
+        </div>
       </div>
-      <div className={classes.section}>
-        <img src={imageOne} className={classes.img} alt="img"></img>
-        <p className={classes.description}>
-         {lorem}
-        </p>
       </div>
-      <div className={classes.section}>
+     
+      <div className={classes.content}>
+         <div className={classes.section}>
         <img src={imageOne} className={classes.img} alt="img"></img>
-        <p className={classes.description}>
-         {lorem}
-        </p>
+        <div className={classes.description}>
+        <p className={classes.text}>{lorem}</p>
+         
+        </div>
+      </div>
+      </div>  <div className={classes.content}>
+         <div className={classes.section}>
+        <img src={imageOne} className={classes.img} alt="img"></img>
+        <div className={classes.description}>
+        <p className={classes.text}>{lorem}</p>
+         
+        </div>
+      </div>
       </div>
     </div>
   );
--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -3,117 +3,57 @@ import Navbar from "../../Navbar/Navbar";
 import img from "./img.jpg";
 import locate from "./location.svg";
 const Projects = () => {
-  return (
-    <div>
-      <Navbar />
+  const project = (
+    <div className={classes.project}>
       <div className={classes.content}>
-      <h1 className={classes.header}>ჩვენს მიერ შესრულებული პროექტები</h1>
+        <img alt="img" className={classes.contentImg} src={img}></img>
+        <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
 
-        <div className={classes.projects}>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button  type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          <div className={classes.project}>
-            <img alt="img" className={classes.contentImg} src={img}></img>
-            <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
-            <div className={classes.location}>
-              <img alt="icon" className={classes.icon} src={locate}></img>
-              <h5>სვანეთი, მესტია</h5>
-            </div>
-            <button type="button" className={classes.moreBtn}>
-             <a href='/project'>სრულად</a>
-            </button>
-          </div>
-          
+        <div className={classes.location}>
+          <img alt="icon" className={classes.icon} src={locate}></img>
+          <h5>სვანეთი, მესტია</h5>
         </div>
+        <button type="button" className={classes.moreBtn}>
+          <a href="/project">სრულად</a>
+        </button>
       </div>
     </div>
+  );
+  return (
+    
+      <div className={classes.main}>
+        {" "}
+        <Navbar />
+        <h1 className={classes.header}>ჩვენს მიერ შესრულებული პროექტები</h1>
+        <div className={classes.projects}>
+          {project}
+          {project}
+          {project}
+          {project}
+          {project}
+          {project}
+          {project}
+          {project}
+          {project}
+        </div>
+      </div>
+   
   );
 };
 
 export default Projects;
+
+//   <div className={classes.project}>
+/* <div className={classes.content}>
+<img alt="img" className={classes.contentImg} src={img}></img>
+<h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
+<div className={classes.location}>
+ <img alt="icon" className={classes.icon} src={locate}></img>
+ <h5>სვანეთი, მესტია</h5>
+</div>
+<button  type="button" className={classes.moreBtn}>
+<a href='/project'>სრულად</a>
+</button>
+</div>
+
+</div> */
