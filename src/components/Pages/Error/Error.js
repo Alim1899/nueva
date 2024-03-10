806e4d1b307dc679ac789a5d4690fb9cebda2fc2
Alim1899 <alimakhchanogli@gmail.com>
2024-03-10 18:36:46 +0400

    Added error page

--- src/components/Pages/Error/Error.js
+++ src/components/Pages/Error/Error.js
@@ -2,10 +2,16 @@ import classes from "./Error.module.css";
 import Navbar from "../../Navbar/Navbar";
 const Error = () => {
   return (
-    <div>
+    <div className={classes.main}>
       <Navbar />
-      <div className={classes.error}>
-        <h1>ERROR PAGE</h1>
+      <div className={classes.content}>
+      <div className={classes.errors}>
+      <h1>დაფიქსირდა შეცდომა!</h1>
+        <button className={classes.button} type="button">
+         <a href="/">მთავარ გვერდზე დაბრუნება</a> 
+        </button>
+      </div>
+        
       </div>
     </div>
   );
