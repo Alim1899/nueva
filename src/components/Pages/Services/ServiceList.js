926776c82eb701c42bd62a891ca8145132dafe1e
Alim1899 <alimakhchanogli@gmail.com>
2024-03-11 02:36:15 +0400

    Update Services

---
+++ src/components/Pages/Services/ServiceList.js
@@ -0,0 +1,100 @@
+import cadastro from "../../../assets/icons/services/cadastro.svg";
+import geology from "../../../assets/icons/services/geology.svg";
+import inside from "../../../assets/icons/services/inside.svg";
+import separate from "../../../assets/icons/services/separate.svg";
+import topo from "../../../assets/icons/services/topo.png";
+import trace from "../../../assets/icons/services/trace.svg";
+import List from "./ServiceList";
+export default function ServiceList () {
+  return  [
+    [
+    { header: "გეოდეზიური მომსახურება" },
+    {
+      subHeaders: [
+        { subhead: "ტერიტორიის დაკვალვა" },
+        { subhead: "ტოპოგრაფიული აზომვა" },
+        { subhead: "შენობის შიდა აზომვა" },
+        { subhead: "ნაყარის მოცულობის დათვლა" },
+        { subhead: "საკადასტრო მომსახურება" },
+      ],
+    },
+    { src: topo },
+  ],
+  [
+    { header: "პროექტირება" },
+    {
+      subHeaders: [
+        { subhead: "საავტომობილო გზები" },
+        { subhead: "სკვერები და პარკები" },
+        { subhead: "წყალმომარაგების ქსელები" },
+        { subhead: "საკანალიზაციო ქსელები" },
+        { subhead: "სანიაღვრე სისტემები" },
+      ],
+    },
+    { src: cadastro },
+  ],
+  [
+    { header: "სტიქიური მოვლენების შეფასება და პრევენცია" },
+    {
+      subHeaders: [
+        { subhead: "საყრდენი კედლების მოწყობა" },
+        { subhead: "ნაპირსამაგრი სამუშაოების პროექტირება" },
+        { subhead: "ქვათაცვენა-კლდეზვავის მოდელირება" },
+        { subhead: "მეწყრების და ღვარცოფების მოდელირება" },
+        { subhead: "პროცესის პერმანენტული მონიტორინგი" },
+      ],
+    },
+    { src: geology },
+  ],
+  [
+    { header: "აგრარული მეურნეობების ანალიზი" },
+    {
+      subHeaders: [
+        { subhead: "ნარგავების რაოდენობის დათვლა" },
+        { subhead: "ნარგავების მდგომარეობის შეფასება " },
+        { subhead: "განაშენიანების ოპტიმალური გეგმის შედგენა" },
+        { subhead: "წყლის პრობლემის/დაჭაობების მიზეზების შესწავლა" },
+      ],
+    },
+    { src: inside },
+  ],
+  [
+    { header: "სამშენებლო სამუშაოების კონსულტირება" },
+    {
+      subHeaders: [
+        { subhead: "სამშენებლო დოკუმენტაციის წარმოება (ფორმა 2)" },
+        { subhead: "სამუშაოს მოცულობების დათვლა  " },
+        { subhead: "სამუშაოების განფასება снип-ებით" },
+        { subhead: "პროექტში კორექტირების პაკეტების მომზადება" },
+        { subhead: "სამშენებლო სამუშაოების ზედამხედველობა" },
+      ],
+    },
+    { src: separate },
+  ],
+  [
+    { header: "პროგრამული ანალიზი  და მოდელირება" },
+    {
+      subHeaders: [
+        { subhead: "მონაცემების სივრცული მოდელირება CIVIL 3D-ში" },
+        { subhead: "მონაცემების ანალიზი ARCGIS PRO-ში  " },
+        { subhead: "ქვათაცვენის მოდელირება ROCKY 3D-ში. " },
+        { subhead: "ორთოფოტოების დამზადება PIX4Dmapper-ში" },
+      ],
+    },
+    { src: trace },
+  ],
+  [
+    { header: "ინდივიდუალური მომსახურება საამზომველო ტექნიკით" },
+    {
+      subHeaders: [
+        { subhead: "GeoCORS სისტემაში ჩართული Stonexის RTK ჯიპიესებით " },
+        { subhead: "საბაზო სადგური  გარე რადიოთი (დაფარვა 50კმ)" },
+        { subhead: "საბაზო სადგური  შიდა რადიოთი (დაფარვა 5კმ)" },
+        { subhead: "Leicaს ბრენდის ტაქეომეტრებით მომსახურება" },
+        { subhead: "აერო გადაღება RTK მულტისპეკტრული დრონით " },
+      ],
+    },
+    { src: List },
+  ],
+]
+}
--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -1,50 +1,29 @@
 import classes from "./Services.module.css";
 import Navbar from "../../Navbar/Navbar";
-import cadastro from "../../../assets/icons/services/cadastro.svg";
-import geology from "../../../assets/icons/services/geology.svg";
-import inside from "../../../assets/icons/services/inside.svg";
-import separate from "../../../assets/icons/services/separate.svg";
-import topo from "../../../assets/icons/services/topo.svg";
-import trace from "../../../assets/icons/services/trace.svg";
+import ServiceList from "./ServiceList";
 const Services = () => {
+  const data = ServiceList();
+console.log(data[0]);
   return (
     <div className={classes.main}>
       <Navbar />
       <div className={classes.services}>
-      <h2 className={classes.heading}>სერვისების ჩამონათვალი</h2>
-        <ul className={classes.list}>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="trace" src={trace}></img>
-            <h4 className={classes.type}>დაკვალვა</h4>
-          
-          </li>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="topo" src={topo}></img>
-            <h4 className={classes.type}>ტოპოგრაფია</h4>
-          
-          </li>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="cadastro" src={cadastro}></img>
-            <h4 className={classes.type}>კადასტრი</h4>
-          
-          </li>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="inside" src={inside}></img>
-            <h4 className={classes.type}>შიდა აზომვა</h4>
-          
-          </li>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="geology" src={geology}></img>
-            <h4 className={classes.type}>გეოლოგიური კვლევა</h4>
-          
-          </li>
-          <li className={classes.listItem}>
-            <img className={classes.icon} alt="separate" src={separate}></img>
-            <h4 className={classes.type}>გამიჯვნა-გაერთიანება</h4>
-          
-          </li>
-        </ul>
-      </div>
+        <h2 className={classes.heading}>სერვისების ჩამონათვალი</h2>
+        <div className={classes.list}>
+          {data.map((el) => {
+            return (
+              <div className={classes.service} key={Math.random()}>
+                <h3 className={classes.serviceHeader}>{el[0].header}</h3>
+                <ul className={classes.unordered}>
+                 {el[1].subHeaders.map(item=>{
+                  return <li className={classes.listItem} key={Math.random()}>{item.subhead}</li>
+                 })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>{" "}
     </div>
   );
 };
