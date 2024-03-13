cd1855226e405229c71dbb2eb2585759ffd8ab89
Alim1899 <alimakhchanogli@gmail.com>
2024-03-13 22:58:42 +0400

    Updated Projects page and added slider

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,13 +1,64 @@
 import classes from "./Projects.module.css";
 import Navbar from "../../Navbar/Navbar";
 import img from "./img.jpg";
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
 import locate from "./location.svg";
 import Footer from "../Footer/Footer";
+import left from "../../../assets/icons/leftslide.svg";
+import right from "../../../assets/icons/rightslide.svg";
+import { Fragment, useState } from "react";
+
 const Projects = () => {
+  const slides = [
+    { id: 1, src: img },
+    { id: 2, src: img1 },
+    { id: 3, src: img2 },
+  ];
+  const [activeSlide, setActiveSlide] = useState(slides[0]);
+  const leftSlide = () => {
+    console.log(activeSlide.id);
+    if (activeSlide.id === 1) {
+      setActiveSlide(slides[2]);
+    } else if (activeSlide.id === 2) {
+      setActiveSlide(slides[0]);
+    } else if (activeSlide.id === 3) {
+      setActiveSlide(slides[1]);
+    }
+  };
+  const rightslide = () => {
+    console.log(activeSlide.id);
+
+    if (activeSlide.id === 1) {
+      setActiveSlide(slides[1]);
+    } else if (activeSlide.id === 2) {
+      setActiveSlide(slides[2]);
+    } else if (activeSlide.id === 3) {
+      setActiveSlide(slides[0]);
+    }
+  };
   const project = (
     <div className={classes.project}>
       <div className={classes.content}>
-        <img alt="img" className={classes.contentImg} src={img}></img>
+        <div className={classes.slider}>
+          <img
+            alt="left"
+            onClick={leftSlide}
+            src={left}
+            className={classes.arrow}
+          ></img>{" "}
+          <img
+            alt="img"
+            className={classes.contentImg}
+            src={activeSlide.src}
+          ></img>
+          <img
+            alt="right"
+            src={right}
+            onClick={rightslide}
+            className={classes.arrow}
+          ></img>{" "}
+        </div>
         <h3 className={classes.projectHeader}>ნენსკრაჰესი - რისკების კვლევა</h3>
 
         <div className={classes.location}>
@@ -21,26 +72,27 @@ const Projects = () => {
     </div>
   );
   return (
-    <div className={classes.main}>
-      {" "}
-      <Navbar />
-      <h1 className={classes.header}>ჩვენს მიერ შესრულებული პროექტები</h1>
-      <div className={classes.projects}>
-        {project}
-        {project}
-        {project}
-        {project}
-        {project}
-        {project}
-        {project}
-        {project}
-        {project}
-      </div>
-      <div className={classes.footer}>
-         <Footer />
-      </div>
-     
-    </div>
+    <Fragment>
+      <div className={classes.main}>
+        {" "}
+        <Navbar />
+        <div className={classes.projects}>
+          <h1 className={classes.header}>ჩვენს მიერ შესრულებული პროექტები</h1>
+          <div className={classes.projectList}>
+            {project}
+            {project}
+            {project}
+            {project}
+            {project}
+            {project}
+            {project}
+            {project}
+            {project}
+          </div>
+        </div>
+      </div>{" "}
+      <Footer />
+    </Fragment>
   );
 };
 
