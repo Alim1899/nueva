4be94560224c169c62862a06a0324434d784567d
Alim1899 <alimakhchanogli@gmail.com>
2024-05-14 00:18:56 +0400

    Updated Services page rendering method

--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -1,58 +1,67 @@
-import classes from "./Services.module.css";
-import ServiceList from "./ServiceList";
+import React, { useState } from "react";
 import down from "../../../assets/icons/services/down.svg";
 import up from "../../../assets/icons/services/up.svg";
+import ServiceList from "./ServiceList";
+import classes from "./Services.module.css";
 
 const Services = () => {
+  const [showExtended, setShowExtended] = useState([]);
+
   const data = ServiceList();
-  const extend = (e) => {
-    const childNodes = e.currentTarget.parentNode.childNodes;
-    childNodes[3].style.display = "block";
-    document.body.style.overflow = "hidden";
+
+  const extend = (index) => {
+    const newShowExtended = [...showExtended];
+    newShowExtended[index] = !newShowExtended[index];
+    setShowExtended(newShowExtended);
   };
-  const collapse = (e) => {
-    const childNodes = e.currentTarget.parentNode.parentNode;
-    childNodes.style.display = "none";
-    document.body.style.overflow = "auto";
-  };
+
   return (
     <div className={classes.main}>
       <div className={classes.services}>
         <h2 className={classes.header}>სერვისების ჩამონათვალი</h2>
         <div className={classes.serviceList}>
-          {data.map((el) => {
+          {data.map((el, index) => {
             return (
-              <div className={classes.service} key={Math.random()}>
-                <img className={classes.icon} src={el[2].src} alt="img"></img>
-                <h3 className={classes.serviceHeader}>{el[0].header}</h3>
-                <button
-                  className={classes.downArrow}
-                  onClick={extend}
-                  type="button"
-                >
-                  <img className={classes.down} alt="down" src={down}></img>
-                </button>
-                <div className={classes.collapse}>
-                  <ul>
-                    {el[1].subHeaders.map((item) => {
-                      return (
-                        <li className={classes.listItem} key={Math.random()}>
-                          {item.subhead}
-                        </li>
-                      );
-                    })}
-                    <button className={classes.upArrow} onClick={collapse}>
-                      <img className={classes.up} src={up} alt="up"></img>{" "}
-                    </button>
-                  </ul>
+              <div className={classes.service} key={index}>
+                <div className={classes.collapsed}>
+                  <img className={classes.icon} src={el[2].src} alt="img" />
+                  <h3 className={classes.serviceHeader}>{el[0].header}</h3>
+                  <button
+                    className={classes.downArrow}
+                    onClick={() => extend(index)}
+                    type="button"
+                  >
+                    <img className={classes.down} alt="down" src={down} />
+                  </button>
                 </div>
+
+                {showExtended[index] && (
+                  <div className={classes.extend}>
+                    <ul>
+                      {el[1].subHeaders.map((item, subIndex) => {
+                        return (
+                          <li className={classes.listItem} key={subIndex}>
+                            {item.subhead}
+                          </li>
+                        );
+                      })}
+                      <button
+                        className={classes.upArrow}
+                        onClick={() => extend(index)}
+                      >
+                        <img className={classes.up} src={up} alt="up" />
+                      </button>
+                    </ul>
+                  </div>
+                )}
               </div>
             );
           })}
         </div>
-      </div>{" "}
+      </div>
     </div>
   );
 };
 
 export default Services;
+
