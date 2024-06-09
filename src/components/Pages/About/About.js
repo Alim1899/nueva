9b206175ab09395f2fdde8622ccd8931337af376
Alim1899 <alimakhchanogli@gmail.com>
2024-06-09 20:07:08 +0400

    Updated About page

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,30 +1,40 @@
 import React from "react";
 import classes from "./About.module.css";
+import { useTranslation } from "react-i18next";
+import kutaisi from '../../../assets/photos/about/Kutaisi.png'
+import drone from '../../../assets/photos/about/drone.png'
+import learn from '../../../assets/photos/about/learn.png'
+import airport from '../../../assets/photos/about/airport.png'
 
-import imageOne from "../../../assets/photos/lazy.jpg";
+
 const About = () => {
-  const lorem =
-    "Officia proident culpa laborum sit enim amet cupidatat. Consequat cillum  consequat reprehenderit laborum consequat et duis. Id reprehenderit dolore et ullamco consequat aliquip. Adipisicing ex laboris aliquip qui amet anim nisi. Lorem laborum labore deserunt eu magna reprehenderit cupidatat culpa veniam magna et amet. Magna est eiusmod excepteur anim elit eu tempor velit sunt excepteur sit cillum anim. Dolore laborum laboris dolor aliquip. Minim magna sint quis irure reprehenderit fugiat fugiat dolore id deserunt est pariatur. Magna culpa dolore laborum Lorem aliqua tempor cillum officia tempor. Mollit ex consequat proident nisi ipsum quis sit reprehenderit ea deserunt deserunt tempor enim enim.";
+  const {t} = useTranslation()
   return (
     <div className={classes.main}>
-  <h1 className={classes.header}>ჩვენს შესახებ</h1>
+  <h1 className={classes.header}>{t('aboutPage.header')}</h1>
       <div className={classes.content}>
         <div className={classes.section}>
-          <img src={imageOne} className={classes.img} alt="img"></img>
+          <img src={kutaisi} className={classes.img} alt="img"></img>
           <div className={classes.description}>
-            <p className={classes.text}>{lorem}</p>
+            <p className={classes.text}>{t('aboutPage.firstParagraph')}</p>
           </div>
         </div>
         <div className={classes.section}>
+        <img src={airport} className={classes.img} alt="img"></img>
           <div className={classes.description}>
-            <p className={classes.text}>{lorem}</p>
+            <p className={classes.text}>{t('aboutPage.secondParagraph')}</p>
           </div>
-          <img src={imageOne} className={classes.img} alt="img"></img>
         </div>
         <div className={classes.section}>
-          <img src={imageOne} className={classes.img} alt="img"></img>
+          <img src={drone} className={classes.img} alt="img"></img>
           <div className={classes.description}>
-            <p className={classes.text}>{lorem}</p>
+            <p className={classes.text}>{t('aboutPage.thirdParagraph')}</p>
+          </div>
+        </div>
+        <div className={classes.section}>
+          <img src={learn} className={classes.img} alt="img"></img>
+          <div className={classes.description}>
+            <p className={classes.text}>{t('aboutPage.fourthParagraph')}</p>
           </div>
         </div>
       </div>
--- src/components/i18n.js
+++ src/components/i18n.js
@@ -8,66 +8,85 @@ const resources = {
   en: {
     translation: {
       main: "Main",
-      about:"About Us",
-      services:"Services",
-      projects:"Projects",
-      gallery:"Gallery",
-      contact:"Contact",
-      homeServices:"We Offer A Different Types Of Services",
-      faq:"Frequently Asked Questions",
-      servicesPage:{
-        header:'SERVICES',
-        serviceList:{
-          geodesy:{
-            header:"Geodetic Service",
-            subHeader1:"Tracing the territory",
-            subHeader2:"Topographic Measurement",
-            subHeader3:"Internal measurement of the building",
-            subHeader4:"Counting bulk volume",
-            subHeader5:"Cadastral services"
-          }
-        }
+      about: "About Us",
+      services: "Services",
+      projects: "Projects",
+      gallery: "Gallery",
+      contact: "Contact",
+      homeServices: "We Offer A Different Types Of Services",
+      faq: "Frequently Asked Questions",
+      servicesPage: {
+        header: "SERVICES",
+        serviceList: {
+          geodesy: {
+            header: "Geodetic Service",
+            subHeader1: "Tracing the territory",
+            subHeader2: "Topographic Measurement",
+            subHeader3: "Internal measurement of the building",
+            subHeader4: "Counting bulk volume",
+            subHeader5: "Cadastral services",
+          },
+        },
       },
-      
-    }
+      aboutPage: {
+        header: "About Us",
+        firstParagraph:
+          'LLC "Nueva" is a design and surveying institution based in the city of Kutaisi. Its working area covers the western region of Georgia. Our clientele includes municipalities of various cities, sub-agencies of the Ministry, educational institutions, and primarily private organizations. The company actively participates in public procurement tenders and has successfully completed hundreds of state orders worth thousands of GEL.',
+        secondParagraph:
+          "Our specialists have been involved in numerous high-complexity projects, including the construction of the new terminal at Kutaisi Airport, the new oil refinery in the village of Kulevi, the new road from Baghdati to Abastumani, the Chiaturi Ropeway, and other similar projects. providing design and geodetic supervision.",
+
+        thirdParagraph:
+          'Our company has also participated in the assessment and mitigation of various types of natural events, and has designed more than 200 retaining walls and over 300 different categories of roads. We own a wide range of the latest material and technical equipment, including Tacheometer, GPS systems, standard and RTK/multi-spectral drones, and the JPI base station with a 50 km operating radius (commonly known as "Base Rovers"). This equipment is used for geodetic studies of construction sites as well as for the analysis of agricultural conditions.',
+
+        fourthParagraph:
+          "LLC \"Nueva\" actively cooperates with professional and higher educational institutions to retrain students and improve training programs. Our specialists have contributed to the development of professional school programs and the enhancement of bachelor's and master's programs in higher education.",
+      },
+    },
   },
   ge: {
     translation: {
-      main:"მთავარი",
-      about:"ჩვენს შესახებ",
-      services:"სერვისები",
-      projects:"პროექტები",
-      gallery:"გალერეა",
-      contact:"კონტაქტი",
-      homeServices:"ჩვენ გთავაზობთ სხვადასხვა ტიპის სერვისს",
-      faq:"ხშირად დასმული კითხვები",
-      servicesPage:{
-        header:'სერვისები',
-        serviceList:{
-          geodesy:{
-            header:"გეოდეზიური მომსახურება",
-            subHeader1:"ტერიტორიის დაკვალვა",
-            subHeader2:"ტოპოგრაფიული აზომვა",
-            subHeader3:"შენობის შიდა აზომვა",
-            subHeader4:"ნაყარის მოცულობის დათვლა",
-            subHeader5:"საკადასტრო მომსახურება"
-          }
-  
-        }
+      main: "მთავარი",
+      about: "ჩვენს შესახებ",
+      services: "სერვისები",
+      projects: "პროექტები",
+      gallery: "გალერეა",
+      contact: "კონტაქტი",
+      homeServices: "ჩვენ გთავაზობთ სხვადასხვა ტიპის სერვისს",
+      faq: "ხშირად დასმული კითხვები",
+      servicesPage: {
+        header: "სერვისები",
+        serviceList: {
+          geodesy: {
+            header: "გეოდეზიური მომსახურება",
+            subHeader1: "ტერიტორიის დაკვალვა",
+            subHeader2: "ტოპოგრაფიული აზომვა",
+            subHeader3: "შენობის შიდა აზომვა",
+            subHeader4: "ნაყარის მოცულობის დათვლა",
+            subHeader5: "საკადასტრო მომსახურება",
+          },
+        },
       },
-      
-    }
-  }
+      aboutPage: {
+        header: "ჩვენს შესახებ",
+        firstParagraph:
+          "შპს „ნუევა“ წარმოადგენს საპროექტო-საამზომველო დაწესებულებას რომელიც ბაზირებულია ქალაქ ქუთაისში, ხოლო მისი სამუშაო არეალი ფარავს დასავლეთ საქართველოს, მისი დამკვეთები არიან სხვადასხვა ქალაქის მუნიციპალიტეტები, სამინისტრო ქვეუწყებები, საგანმანათლებლო დაწესებულებები და მეტწილად კერძო ორგანიზაციები. კომპანიები აქტიურად მონაწილეობს სახელმწიფო შესყიდვების ფარგლებში გამოცხადებულ ტენდერებში და წარმატებით შესრულებული აქვს ასეულ ათასობით ლარის სახელმწიფო დაკვეთები. ",
+        secondParagraph:
+          "ჩვენი სპეციალისტები ჩართულები იყვნენ ქუთაისის აეროპორტის ახალი ტერმინალის, სოფელ ყულევში ახალი ნავთობგადამამუშავებელი ქარხნის, ბაღდათი-აბასთუმნის ახალი გზის , ჭიათურის საშევარდნოს გადმოსახედის და სხვა მაღალი სირთულის ობიექტის მშენებლობის პროცესში, პროექტირების და გეოდეზიური ზედამხედველობის მხრივ. ",
+        thirdParagraph:
+          'ჩვენი კომპანია მონაწილეობდა სხვადასხვა ტიპის სტიქიური მოვლენების შეფასება-ლიკვიდაციის სამუშაოებში და დაპროექტებული აქვს 200 ზე მეტი საყრდენი კედელი და 300 ზე მეტი სხვადასხვა კატეგორიის გზა. კომპანია ფლობს ფართო სპექტრის უახლეს მატერიალურ-ტექნიკურ ბაზას, რაც მოიცავს ტაქეომეტრებს, ჯიპისებს, სტანდარტულ და რტკ /მულტისპქტრულ დრონებს, 50 კმ რადიუსზე მოქმედ ჯიპიეს საბაზო სადგურს (ე.წ. "ბაზა როვერს"). ამ ტექნიკის გამოყენებით ხდება როგორც სამშენებლო ობიექტების გეოდეზიური შესწავლა, ასევე სასოფლო სამეურნეო ობიექტების მდგომარეობის ანალიზი. ',
+        fourthParagraph:
+          'შპს "ნუევა" აქტიურად თანამშრომლობს პროფესიულ და უმაღლეს სასწავლებლებთან, სტუდენტების გადამზადებისა და სასწავლო პროგრამების დახვეწის კუთხით. სხვადასხვა დროს მისი სპეციალისტები მონაწილეობდნენ პროფესიული სასწავლებლების პროგრამებისა შემუშავებასა და უმაღლესი სასწავლებლის საბაკალავრო და სამაგისტრო პროგრამების დახვეწაში.',
+      },
+    },
+  },
 };
 
-i18n
-  .use(initReactI18next) 
-  .init({
-    resources,
-    lng: sessionStorage.getItem("lng")||"ge", 
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: sessionStorage.getItem("lng") || "ge",
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
 
-  export default i18n;
+export default i18n;
