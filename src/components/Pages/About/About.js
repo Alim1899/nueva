a6deed9825c823fcd28bcb890552387563f3b421
Alim1899 <alimakhchanogli@gmail.com>
2024-07-08 17:42:41 +0400

    Updated about and gallery page sources

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./About.module.css";
 import { useTranslation } from "react-i18next";
-import kutaisi from '../../../assets/photos/about/Kutaisi.png'
-import drone from '../../../assets/photos/about/drone.png'
-import learn from '../../../assets/photos/about/learn.png'
-import airport from '../../../assets/photos/about/airport.png'
+import kutaisi from '../../../assets/photos/about/2.jpg'
+import drone from '../../../assets/photos/about/1.jpeg'
+import learn from '../../../assets/photos/about/3.jpeg'
+import airport from '../../../assets/photos/about/4.jpeg'
 
 
 const About = () => {
@@ -14,25 +14,25 @@ const About = () => {
   <h1 className={classes.header}>{t('aboutPage.header')}</h1>
       <div className={classes.content}>
         <div className={classes.section}>
-          <img src={kutaisi} className={classes.img} alt="img"></img>
+          <img src={airport} className={classes.img} alt="img"></img>
           <div className={classes.description}>
             <p className={classes.text}>{t('aboutPage.firstParagraph')}</p>
           </div>
         </div>
         <div className={classes.section}>
-        <img src={airport} className={classes.img} alt="img"></img>
+        <img src={kutaisi} className={classes.img} alt="img"></img>
           <div className={classes.description}>
             <p className={classes.text}>{t('aboutPage.secondParagraph')}</p>
           </div>
         </div>
         <div className={classes.section}>
-          <img src={drone} className={classes.img} alt="img"></img>
+          <img src={learn} className={classes.img} alt="img"></img>
           <div className={classes.description}>
             <p className={classes.text}>{t('aboutPage.thirdParagraph')}</p>
           </div>
         </div>
         <div className={classes.section}>
-          <img src={learn} className={classes.img} alt="img"></img>
+          <img src={drone} className={classes.img} alt="img"></img>
           <div className={classes.description}>
             <p className={classes.text}>{t('aboutPage.fourthParagraph')}</p>
           </div>
--- src/components/Pages/Gallery/Gallery.js
+++ src/components/Pages/Gallery/Gallery.js
@@ -1,8 +1,16 @@
 import classes from "./Gallery.module.css";
-import img from "../../../assets/photos/lazy.jpg";
-import img1 from "../../../assets/photos/scr1.png";
-import img2 from "../../../assets/photos/scr2.png";
-import img3 from "../../../assets/photos/scr3.png";
+import img1 from "../../../assets/photos/gallery/1.jpg"
+import img2 from "../../../assets/photos/gallery/2.jpg"
+import img3 from "../../../assets/photos/gallery/3.jpg"
+import img4 from "../../../assets/photos/gallery/4.jpg"
+import img5 from "../../../assets/photos/gallery/5.jpg"
+import img6 from "../../../assets/photos/gallery/6.jpg"
+import img7 from "../../../assets/photos/gallery/7.jpg"
+import img8 from "../../../assets/photos/gallery/8.jpg"
+import img9 from "../../../assets/photos/gallery/9.jpg"
+import img10 from "../../../assets/photos/gallery/10.jpg"
+import img11 from "../../../assets/photos/gallery/11.jpg"
+import img12 from "../../../assets/photos/gallery/12.jpg"
 import { useTranslation } from "react-i18next";
 const Gallery = () => {
   const {t}= useTranslation();
@@ -10,18 +18,18 @@ const Gallery = () => {
     <div className={classes.main}>
       <h2 className={classes.header}>{t("galleryPage.header")}</h2>
       <div className={classes.photos}>
-        <img alt="img" className={classes.img} src={img}></img>
         <img alt="img" className={classes.img} src={img1}></img>
         <img alt="img" className={classes.img} src={img2}></img>
         <img alt="img" className={classes.img} src={img3}></img>
-        <img alt="img" className={classes.img} src={img}></img>
-        <img alt="img" className={classes.img} src={img1}></img>
-        <img alt="img" className={classes.img} src={img2}></img>
-        <img alt="img" className={classes.img} src={img3}></img>
-        <img alt="img" className={classes.img} src={img}></img>
-        <img alt="img" className={classes.img} src={img1}></img>
-        <img alt="img" className={classes.img} src={img2}></img>
-        <img alt="img" className={classes.img} src={img3}></img>
+        <img alt="img" className={classes.img} src={img4}></img>
+        <img alt="img" className={classes.img} src={img5}></img>
+        <img alt="img" className={classes.img} src={img6}></img>
+        <img alt="img" className={classes.img} src={img7}></img>
+        <img alt="img" className={classes.img} src={img8}></img>
+        <img alt="img" className={classes.img} src={img9}></img>
+        <img alt="img" className={classes.img} src={img10}></img>
+        <img alt="img" className={classes.img} src={img11}></img>
+        <img alt="img" className={classes.img} src={img12}></img>
       </div>
     </div>
   );
